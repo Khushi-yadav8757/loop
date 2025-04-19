5b2fe9c83e37928d6eb05d0ac00a917363e90c03
Khushi-yadav8757 <kky957242@gmail.com>
2025-04-19 17:21:58 +0530

    first commit

---
+++ string.py
@@ -0,0 +1,10 @@
+#Single Quote in String
+print('Hello World')
+#Double Quote in string
+print("Hello Khushi")
+# Multiline string
+
+a = """Python is the most popular language in the IT industries
+python is userfriendly and very easy to implement like English language
+it is used in Ai,Machine language,Data science"""
+print(a)
